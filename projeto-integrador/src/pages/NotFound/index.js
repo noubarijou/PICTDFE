8b7e47198806b1b51700d4b47a80ddefb9099573
noubarijou <85699669+noubarijou@users.noreply.github.com>
2021-12-01 12:31:16 -0300

    imagens

--- projeto-integrador/src/pages/NotFound/index.js
+++ projeto-integrador/src/pages/NotFound/index.js
@@ -1,5 +1,6 @@
 import {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
+import {Helmet} from 'helmet-react';
 
 const NotFound = () => {
     const [timer, setTimer] = useState(5);
