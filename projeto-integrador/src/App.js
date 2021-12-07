d66ee267108c7e447383107bec4f17066e987aa2
luizinbrzado <luiztrineves@gmail.com>
2021-12-07 00:03:07 -0300

    Parte do home pronta

--- projeto-integrador/src/App.js
+++ projeto-integrador/src/App.js
@@ -1,5 +1,5 @@
 import RouteList from './routes';
-import './App.css';
+import './App.scss';
 
 function App() {
 
--- projeto-integrador/src/pages/Home/index.js
+++ projeto-integrador/src/pages/Home/index.js
@@ -1,5 +1,8 @@
 // import React from 'react'
+import { Link } from "react-router-dom";
+import { Button, Carousel, Col, Row, Container } from "react-bootstrap";
 import useAxios from "../../Hooks/useAxios";
+import './style.scss'
 
 const Home = () => {
 
@@ -7,14 +10,24 @@ const Home = () => {
 
     return (
         <>
-            {/* {
-                jogos.map((jogo, index) => {
-                    return (
-                        <h1 key={index}>{jogo.titulo}</h1>
-                    )
-                })
-
-            } */}
+            <section className="parallax">
+                <article className="apresentacao text-center mx-4">
+                    <h2>A melhor loja de games da nossa rua</h2>
+                    <a href="#jogos"><Button variant="success" className="text-uppercase">Confira</Button></a>
+                </article>
+                <section className="recomendados">
+                    <h1>RECOMENDADOS</h1>
+                </section>
+            </section>
+            <section id="jogos">
+                {
+                    jogos.map((jogo, index) => {
+                        return (
+                            <h1 key={index+1}>{jogo.titulo}</h1>
+                        )
+                    })
+                }
+            </section>
         </>
     )
 }
