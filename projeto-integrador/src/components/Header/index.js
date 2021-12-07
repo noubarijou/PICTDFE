2b6fed470a2b0f3ce03bf92d03bcd764edff974f
jennimay <mayumi.jenni@gmail.com>
2021-12-07 00:20:08 -0300

    logo e favicon

--- projeto-integrador/src/components/Header/index.js
+++ projeto-integrador/src/components/Header/index.js
@@ -1,28 +1,30 @@
 import React from 'react'
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import {Link} from 'react-router-dom';
+import logo from '../../assets/img/logo-poison-modelo1.png'
+import './style.scss'
 
-const Footer = () => {
+const Header = () => {
     return (
         <>
            <header>
-                <Navbar bg="dark" variant="dark" expand="lg">
-                    <Container>
-                        <Navbar.Brand href="/">
+                <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+                    <Container fluid>
+                        <Navbar.Brand href="/" id="brand">
                             <img
-                                alt=""
-                                src="/logo.svg"
-                                width="30"
-                                height="30"
+                                alt="Cogumelo roxo com bolinhas verdes e olhos de ponta cabeça"
+                                src={logo}
+                                height="40"
+                                id="logo"
                                 className="d-inline-block align-top"
                             />{' '}
                             Poison games
                         </Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
+                            <Nav className="mx-auto">
                                 <Nav.Link><Link to="/jogos">Jogos</Link></Nav.Link>
-                                <NavDropdown title="Categorias" id="basic-nav-dropdown">
+                                <NavDropdown title="Categorias" id="basic-nav-dropdown" variant="dark">
                                     <NavDropdown.Item><Link to="/categorias/1">Ação</Link></NavDropdown.Item>
                                     <NavDropdown.Item><Link to="/categorias/2">RPG</Link></NavDropdown.Item>
                                     <NavDropdown.Item><Link to="/categorias/3">Estratégia</Link></NavDropdown.Item>
@@ -33,6 +35,8 @@ const Footer = () => {
                                 </NavDropdown>
                                 <Nav.Link><Link to="/sobre">Sobre</Link></Nav.Link>
                                 <Nav.Link><Link to="/suporte">Suporte</Link></Nav.Link>
+                            </Nav>
+                            <Nav>
                                 <NavDropdown title="Entrar" id="basic-nav-dropdown">
                                     <NavDropdown.Item><Link to="/login">Login</Link></NavDropdown.Item>
                                     <NavDropdown.Item><Link to="/cadastrar">Cadastrar</Link></NavDropdown.Item>
@@ -49,4 +53,4 @@ const Footer = () => {
     )
 }
 
-export default Footer
+export default Header;
--- projeto-integrador/src/routes/index.js
+++ projeto-integrador/src/routes/index.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Footer from '../components/Footer'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Home from '../pages/Home';
