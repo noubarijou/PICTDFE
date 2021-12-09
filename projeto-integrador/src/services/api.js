98397b0fb5d738ac76c5162238e438383b5442be
luizinbrzado <luiztrineves@gmail.com>
2021-12-09 08:47:55 -0300

    Trocando link da api

--- projeto-integrador/src/services/api.js
+++ projeto-integrador/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://ec2-54-157-41-166.compute-1.amazonaws.com:8080'
+  // baseURL: 'http://ec2-54-157-41-166.compute-1.amazonaws.com:8080'
+  baseURL: 'https://poisongames.herokuapp.com'
 });
 
 export default api;
