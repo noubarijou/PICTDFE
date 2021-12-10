f4710873766ad57c31e1f5231d6e34f33665349c
jennimay <mayumi.jenni@gmail.com>
2021-12-10 16:57:00 -0300

    responsividade finalizada, talvez?

--- projeto-integrador/src/pages/Sobre/membros.js
+++ projeto-integrador/src/pages/Sobre/membros.js
@@ -4,35 +4,35 @@ let Membros = [
         nome: "Luiz Gustavo",
         foto: "https://cdn.discordapp.com/attachments/889532130495975446/918875578029977650/amigosDoAndre1.png",
         github: "https://github.com/luizinbrzado",
-        githubImg: "https://cdn.discordapp.com/attachments/872490351561150474/918883011817259038/github-luiz.png",
+        githubImg: "https://cdn.discordapp.com/attachments/872490351561150474/918938370590277632/github-luiz2.png",
         linkedin: "https://www.linkedin.com/in/luizinbrzado"
     },
     {
-        nome: "Paulo Ventrua",
+        nome: "Paulo Ventura",
         foto: "https://media.discordapp.net/attachments/889532130495975446/918875604537966612/amigosDoAndre2.png?width=460&height=473",
         github: "https://github.com/noubarijou",
-        githubImg: "https://cdn.discordapp.com/attachments/872490351561150474/918883012140224512/github-paulo.png",
+        githubImg: "https://cdn.discordapp.com/attachments/872490351561150474/918938370305036309/github-paulo2.png",
         linkedin: "https://www.linkedin.com/in/paulo-ventura-50079913/"
     },
     {
         nome: "André Ribeiro",
         foto: "https://cdn.discordapp.com/attachments/889532130495975446/918608255138279504/amigosDaGalera.jpg",
         github: "https://github.com/AndreRibeiro07",
-        githubImg: "https://cdn.discordapp.com/attachments/872490351561150474/918881143070294076/github-andre.png",
+        githubImg: "https://cdn.discordapp.com/attachments/872490351561150474/918938370896441434/github-andre2.png",
         linkedin: "https://www.linkedin.com/in/andrerbr/"
     },
     {
         nome: "Jennifer Mayumi",
         foto: "https://media.discordapp.net/attachments/889532130495975446/915710398106329149/amigosDoAndre4.png?width=460&height=473",
         github: "https://github.com/jennimay",
-        githubImg: "https://cdn.discordapp.com/attachments/872490351561150474/918881142860562493/github-jenni.png",
+        githubImg: "https://cdn.discordapp.com/attachments/872490351561150474/918938371206811658/github-jenni2.png",
         linkedin: "https://www.linkedin.com/in/mundodajeje"
     },
     {
         nome: "Nelson Kobayashi",
         foto: "https://media.discordapp.net/attachments/889532130495975446/918876090628472933/amigosDoAndre3.png?width=460&height=473",
         github: "https://github.com/NelsonKobayashi",
-        githubImg: "https://cdn.discordapp.com/attachments/872490351561150474/918883012496728094/github-nelson.png",
+        githubImg: "https://cdn.discordapp.com/attachments/872490351561150474/918938370024038410/github-nelson2.png",
         linkedin: "https://www.linkedin.com/in/nelsonkobayashi"
     }
     /* {
