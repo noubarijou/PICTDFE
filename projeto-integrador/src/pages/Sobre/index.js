359f2b15c4b79e94d374bf928581867c9a5e479f
jennimay <mayumi.jenni@gmail.com>
2021-12-10 14:26:24 -0300

    responsividade da pagina sobre

--- projeto-integrador/src/pages/Sobre/index.js
+++ projeto-integrador/src/pages/Sobre/index.js
@@ -1,4 +1,5 @@
-import { Github, Linkedin } from "react-bootstrap-icons";
+import { Linkedin } from "react-bootstrap-icons";
+import Membros from "./membros.js";
 import "./style.scss";
 
 const Sobre = () => (
@@ -6,141 +7,49 @@ const Sobre = () => (
     <h1>Sobre</h1>
     <div className="linha mb-4"></div>
     <article className="texticulo">
-      <p>A Poison Games nasceu da ideia de oferecer ao mundo gamer uma plataforma limpa e direta, acesso aos melhores jogos e com preços acessíveis - mesmo que a loja seja fictícia. A equipe da Poison Games é formada originalmente por 3 dos membros que já haviam feito alguns trabalhos juntos. Adicionando uma mestra em front-end e um jovem padawan, a equipe ficou completa.</p>
-      <p>Em pouco mais de uma semana, aplicamos o conteúdo proposto até agora pelo CTD da DH, e o resultado é esse que você pode ver. Com uma abordagem de 'funciona na minha máquina', muitas sessões no Discord lotadas de péssimas piadas (mas muitas risadas) e alguns eventos que nos obrigaram a começar do zero concluímos essa fase do curso com essa aplicação.</p>
-      <p>Agradecemos aos professores pela paciência (especialmente com as anteriormente mencionadas piadas sem graça) e dedicação ao nosso aprendizado. E um valeu em especial pra nossa querida tutora Vi, que diferente dos professores, nos aguenta durante pelo menos 6h por dia há mais de 4 meses - mas nos acompanha desde março de 2021. Fica aqui o nosso agradecimento!</p>
-      <p>Qualquer dúvida, não entre em contato. Qualquer coisa que pareça interessante, pode pegar o código!
-        Se tem uma coisa que aprendemos nessa caminhada, é que sem o código alheio, fica quase impossível aprender.</p>
+      <p>
+        A Poison Games nasceu da ideia de oferecer ao mundo gamer uma plataforma limpa e direta, acesso aos melhores jogos e com preços acessíveis - mesmo que a loja seja fictícia. A equipe da Poison Games é formada originalmente por 3 dos membros que já haviam feito alguns trabalhos juntos. Adicionando uma mestra em front-end e um jovem padawan, a equipe ficou completa.
+      </p>
+      <p>
+        Em pouco mais de uma semana, aplicamos o conteúdo proposto até agora pelo CTD da DH, e o resultado é esse que você pode ver. Com uma abordagem de 'funciona na minha máquina', muitas sessões no Discord lotadas de péssimas piadas (mas muitas risadas) e alguns eventos que nos obrigaram a começar do zero concluímos essa fase do curso com essa aplicação.
+      </p>
+      <p>
+        Agradecemos aos professores pela paciência (especialmente com as anteriormente mencionadas piadas sem graça) e dedicação ao nosso aprendizado. E um valeu em especial pra nossa querida tutora Vi, que diferente dos professores, nos aguenta durante pelo menos 6h por dia há mais de 4 meses - mas nos acompanha desde março de 2021. Fica aqui o nosso agradecimento!
+      </p>
+      <p>
+        Qualquer dúvida, não entre em contato. Qualquer coisa que pareça interessante, pode pegar o código!
+        Se tem uma coisa que aprendemos nessa caminhada, é que sem o código alheio, fica quase impossível aprender.
+      </p>
       <div className="linha mb-4"></div>
-      <p className="assinatura"><i>Equipe Poison Games - a melhor loja de jogos da sua rua.</i></p>
+      <p className="assinatura">
+        <i>Equipe Poison Games - a melhor loja de jogos da sua rua.</i>
+      </p>
     </article>
     <article className="card-membros">
-      <div>
-        <img
-          src="https://cdn.discordapp.com/attachments/889532130495975446/916049906005848134/amigosDoAndreTulio.png"
-          alt="amigosdoandre"
-        ></img>
-        <p>Luiz Gustavo</p>
-        <div className="linha mb-3"></div>
-        <div>
-          <a
-            href="https://github.com/luizinbrzado"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            <Github color="white" size={30} />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/luizinbrzado"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            <Linkedin color="white" size={30} />{" "}
-          </a>
-        </div>
-      </div>
-      <div>
-        <img
-          src="https://cdn.discordapp.com/attachments/889532130495975446/916049906005848134/amigosDoAndreTulio.png"
-          alt="amigosdoandre"
-        ></img>
-        <p>Paulo Ventura</p>
-        <div className="linha mb-3"></div>
-        <div>
-          <a
-            href="https://github.com/noubarijou"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            <Github color="white" size={30} />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/paulo-ventura-50079913/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            <Linkedin color="white" size={30} />
-          </a>
-        </div>
-      </div>
-      <div>
-        <img
-          src="https://cdn.discordapp.com/attachments/889532130495975446/916049906005848134/amigosDoAndreTulio.png"
-          alt="amigosdoandre"
-        ></img>
-        <p>André Ribeiro</p>
-        <div className="linha mb-3"></div>
-        <div>
-          <a
-            href="https://github.com/AndreRibeiro07"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            <Github color="white" size={30} />
-          </a>
-          <a href="https://linkedin.com/" target="_blank" rel="noreferrer">
-            {" "}
-            <Linkedin color="white" size={30} />
-          </a>
-        </div>
-      </div>
-      <div>
-        <img
-          src="https://cdn.discordapp.com/attachments/889532130495975446/916049906005848134/amigosDoAndreTulio.png"
-          alt="amigosdoandre"
-        ></img>
-        <p>Jennifer Mayumi</p>
-        <div className="linha mb-3"></div>
-        <div>
-          <a
-            href="https://github.com/jennimay"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            <Github color="white" size={30} />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/mundodajeje"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            <Linkedin color="white" size={30} />
-          </a>
-        </div>
-      </div>
-      <div>
-        <img
-          src="https://cdn.discordapp.com/attachments/889532130495975446/916049906005848134/amigosDoAndreTulio.png"
-          alt="amigosdoandre"
-        ></img>
-        <p>Nelson Kobayashi</p>
-        <div className="linha mb-3"></div>
-        <div>
-          <a
-            href="https://github.com/NelsonKobayashi"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            <Github color="white" size={30} />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/nelsonkobayashi"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            <Linkedin color="white" size={30} />
-          </a>
-        </div>
-      </div>
+      {
+        Membros.map((integrante) => {
+
+          return (
+            <div className="membro">
+              <div className="foto-perfil">
+                <img src={integrante.foto} alt={integrante.nome} />
+              </div>
+              <div className="informacoes">
+                <p>{integrante.nome}</p>
+                <div className="linha mb-3"></div>
+                <div className="redes-sociais">
+                  <a href={integrante.github} target="_blank" rel="noreferrer">
+                    <img src={integrante.githubImg} alt="GitHub personalizado com o rosto de cada integrante" />
+                  </a>
+                  <a href={integrante.linkedin} target="_blank" rel="noreferrer">
+                    <Linkedin color="white" size={40} />
+                  </a>
+                </div>
+              </div>
+            </div>
+          )
+        })
+      }
     </article>
   </section>
 );
---
+++ projeto-integrador/src/pages/Sobre/membros.js
@@ -0,0 +1,47 @@
+
+let Membros = [
+    {
+        nome: "Luiz Gustavo",
+        foto: "https://cdn.discordapp.com/attachments/889532130495975446/918875578029977650/amigosDoAndre1.png",
+        github: "https://github.com/luizinbrzado",
+        githubImg: "https://cdn.discordapp.com/attachments/872490351561150474/918883011817259038/github-luiz.png",
+        linkedin: "https://www.linkedin.com/in/luizinbrzado"
+    },
+    {
+        nome: "Paulo Ventrua",
+        foto: "https://media.discordapp.net/attachments/889532130495975446/918875604537966612/amigosDoAndre2.png?width=460&height=473",
+        github: "https://github.com/noubarijou",
+        githubImg: "https://cdn.discordapp.com/attachments/872490351561150474/918883012140224512/github-paulo.png",
+        linkedin: "https://www.linkedin.com/in/paulo-ventura-50079913/"
+    },
+    {
+        nome: "André Ribeiro",
+        foto: "https://cdn.discordapp.com/attachments/889532130495975446/918608255138279504/amigosDaGalera.jpg",
+        github: "https://github.com/AndreRibeiro07",
+        githubImg: "https://cdn.discordapp.com/attachments/872490351561150474/918881143070294076/github-andre.png",
+        linkedin: "https://www.linkedin.com/in/andrerbr/"
+    },
+    {
+        nome: "Jennifer Mayumi",
+        foto: "https://media.discordapp.net/attachments/889532130495975446/915710398106329149/amigosDoAndre4.png?width=460&height=473",
+        github: "https://github.com/jennimay",
+        githubImg: "https://cdn.discordapp.com/attachments/872490351561150474/918881142860562493/github-jenni.png",
+        linkedin: "https://www.linkedin.com/in/mundodajeje"
+    },
+    {
+        nome: "Nelson Kobayashi",
+        foto: "https://media.discordapp.net/attachments/889532130495975446/918876090628472933/amigosDoAndre3.png?width=460&height=473",
+        github: "https://github.com/NelsonKobayashi",
+        githubImg: "https://cdn.discordapp.com/attachments/872490351561150474/918883012496728094/github-nelson.png",
+        linkedin: "https://www.linkedin.com/in/nelsonkobayashi"
+    }
+    /* {
+        nome:"",
+        foto:"",
+        github:"",
+        githubImg:"",
+        linkedin:""
+    } */
+];
+
+export default Membros;
