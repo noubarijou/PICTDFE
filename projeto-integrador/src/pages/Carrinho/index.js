67192c1a1c51a8ed92f270dc9fdaed97dc877476
nelsonkobayashi <koba.nelson@gmail.com>
2021-12-10 13:26:03 -0300

    ajeitando carrinho, tobe continued

--- projeto-integrador/src/pages/Carrinho/index.js
+++ projeto-integrador/src/pages/Carrinho/index.js
@@ -1,6 +1,6 @@
 import Helmet from 'react-helmet';
-import {useState, useEffect, useContext} from 'react';
-import {Container} from 'react-bootstrap';
+import { useState, useEffect, useContext } from 'react';
+import { Container } from 'react-bootstrap';
 import { CarrinhoContext } from '../../context/jogoContext';
 import './style.scss';
 import useAxios from '../../Hooks/useAxios';
@@ -9,25 +9,26 @@ import PrecoNoCarrinho from './component/PrecoNoCarrinho';
 
 
 const Carrinho = () => {
-    const {carrinho} = useContext(CarrinhoContext);
+    const { carrinho } = useContext(CarrinhoContext);
     const jogos = useAxios(`/produto`);
-    
+
     return (
         <>
-        <Helmet>
-            <title>Poison Games | Carrinho</title>
-        </Helmet>
-        <Container id="carrinho">
-        <h1>Carrinho</h1>
-        
-        {carrinho.length ? 
-        (carrinho.map((e)=> jogos.filter((item, index)=>item.id===e.id).map((i)=> <PrecoNoCarrinho key={e.id} id={e.id} titulo={i.titulo} imagem={i.imagem} preco={i.preco}/>))    
-        ):(
-        <div className="linha mb-4">FORTALECE AÍ POW</div>
-        )
+            <Helmet>
+                <title>Poison Games | Carrinho</title>
+            </Helmet>
+            <Container id="carrinho">
+                <h1>Carrinho</h1>
+                <div className="linha"></div>
 
-    }
-     </Container>    
+                {carrinho.length ?
+                    (carrinho.map((e) => jogos.filter((item, index) => item.id === e.id).map((i) => <PrecoNoCarrinho key={e.id} id={e.id} titulo={i.titulo} imagem={i.imagem} preco={i.preco} />))
+                    ) : (
+                        <div className="mb-4">FORTALECE AÍ POW</div>
+                    )
+
+                }
+            </Container>
         </>
     )
 }
